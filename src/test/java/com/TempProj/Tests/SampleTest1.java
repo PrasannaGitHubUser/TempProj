02ec85bbc3892eb6693495cbadab6b19c8e750e2
PrasannaGitHubUser <azure1.prasanna@gmail.com>
2020-08-09 18:08:34 +0530

    First commit to upload code into git repo

---
+++ src/test/java/com/TempProj/Tests/SampleTest1.java
@@ -0,0 +1,70 @@
+package com.TempProj.Tests;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SampleTest1 {
+
+	@Test
+	public void testNG1() {
+		System.out.println("testNG1");
+	}
+	
+	@Test
+	public void testNG2() {
+		System.out.println("testNG2");
+	}
+	
+	@Test
+	public void testNG3() {
+		System.out.println("testNG3");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("beforeMethod");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("afterMethod\n");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("beforeClass\n");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("afterClass");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("beforeTest");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("afterTest");
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("beforeSuite");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("afterSuite");
+	}
+
+}
